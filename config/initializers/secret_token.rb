fafb51ff1a1420ee71532127cdbe37bcb407903e
Lou Zell <lzell11@gmail.com>
2013-05-09 17:19:19 -0700

    Make secret token and session_store_key overwriteable on deployment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OKDashboard::Application.config.secret_token = 'a50bc4680892aff8ad21f672cb804c47356e04842d7c64abb834f95a8851433c2774eec1b7a36d1ae6c6a78b9231853f9bdb7e23e9a605a7bc0aec0b44a0bc7e'
+OKDashboard::Application.config.secret_token = OKConfig[:rails_secret_token]
