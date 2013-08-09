3083e1b8686cf2c74eefa960e9d9991c812514e2
Lou Zell <lzell11@gmail.com>
2013-08-09 06:01:29 -0700

    Removed tracer_bullets that were left in by accident

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -39,11 +39,9 @@ class ScoresController < ApplicationController
       @score = @leaderboard.scores.build(params[:score])
       @score.value = value
       @score.user = user
-      tracer_bullet
       if !@score.save
         err_message = "#{@score.errors.full_messages.join(", ")}"
       end
-      tracer_bullet
     end
 
     if err_message
