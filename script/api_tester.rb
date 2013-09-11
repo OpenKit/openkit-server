0537d1943b46f8c579a510efe7917c3239c98908
Lou Zell <lzell11@gmail.com>
2013-09-11 11:57:39 -0700

    Updated api_tester to hit v1 path

--- script/api_tester.rb
+++ script/api_tester.rb
@@ -65,62 +65,62 @@ end
 
 # Makes a special request to purge data associated with end_to_end_test app.  Hack.  In the future
 # we'll have a test instance up to blast with data.
-res = delete '/purge_test_data'
+res = delete '/v1/purge_test_data'
 if res.code != "200"
   puts "Could not purge test data, maybe you do not have an app with app key #{KEY} on #{EP}?"
   exit 1
 end
 
 # Create some users
-res1 = post '/users',              { user: {nick: "test user 1", custom_id: 454} }
-res2 = post '/users',              { user: {nick: "test user 2", custom_id: 600} }
+res1 = post '/v1/users',              { user: {nick: "test user 1", custom_id: 454} }
+res2 = post '/v1/users',              { user: {nick: "test user 2", custom_id: 600} }
 user1 = JSON.parse(res1.body)
 user2 = JSON.parse(res2.body)
 
 # Create a couple leaderboards
-res1 = post '/leaderboards',       { leaderboard: {name: 'leader 1', sort_type: 'LowValue', tag_list:"v1"} }
-res2 = post '/leaderboards',       { leaderboard: {name: 'leader 2', sort_type: 'HighValue', tag_list:"v1,v2"} }
+res1 = post '/v1/leaderboards',       { leaderboard: {name: 'leader 1', sort_type: 'LowValue', tag_list:"v1"} }
+res2 = post '/v1/leaderboards',       { leaderboard: {name: 'leader 2', sort_type: 'HighValue', tag_list:"v1,v2"} }
 low_board = JSON.parse(res1.body)
 high_board = JSON.parse(res2.body)
 
 # List leaderboards
-get '/leaderboards'
+get '/v1/leaderboards'
 
 # List leaderboards with a certain tag:
-get '/leaderboards',               { tag: "v2" }
+get '/v1/leaderboards',               { tag: "v2" }
 
 # Show a leaderboard
-get "/leaderboards/#{high_board['id']}"
+get "/v1/leaderboards/#{high_board['id']}"
 
 # Create a couple achievements
-res1 = post '/achievements',       { achievement: {name: 'achievement 1', desc: "Get x foos and receive a bar", points: 100, goal: 5 } }
-res2 = post '/achievements',       { achievement: {name: 'achievement 2', desc: "Take some skins, jazz begins", points: 200, goal: 5 } }
+res1 = post '/v1/achievements',       { achievement: {name: 'achievement 1', desc: "Get x foos and receive a bar", points: 100, goal: 5 } }
+res2 = post '/v1/achievements',       { achievement: {name: 'achievement 2', desc: "Take some skins, jazz begins", points: 200, goal: 5 } }
 ach1 = JSON.parse(res1.body)
 ach2 = JSON.parse(res2.body)
 
 # User 1 plays
-post '/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user1['id'], value: 9, display_string: "9 seconds!"} }
-post '/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user1['id'], value: 10, display_string: "10 seconds!"} }
-post '/achievement_scores',        { achievement_score: {achievement_id: ach1['id'], user_id: user1['id'], progress: 1}}
-post '/achievement_scores',        { achievement_score: {achievement_id: ach1['id'], user_id: user1['id'], progress: 4}}
+post '/v1/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user1['id'], value: 9, display_string: "9 seconds!"} }
+post '/v1/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user1['id'], value: 10, display_string: "10 seconds!"} }
+post '/v1/achievement_scores',        { achievement_score: {achievement_id: ach1['id'], user_id: user1['id'], progress: 1}}
+post '/v1/achievement_scores',        { achievement_score: {achievement_id: ach1['id'], user_id: user1['id'], progress: 4}}
 
 # User 2 plays
-post '/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user2['id'], value: 6, display_string: "6 seconds!"} }
-post '/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user2['id'], value: 12, display_string: "12 seconds!"} }
+post '/v1/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user2['id'], value: 6, display_string: "6 seconds!"} }
+post '/v1/scores',                    { score: {leaderboard_id: low_board['id'], user_id: user2['id'], value: 12, display_string: "12 seconds!"} }
 
 # Get user1 best
-get '/best_scores/user',           { leaderboard_id: low_board['id'], user_id: user1['id'] }
+get '/v1/best_scores/user',           { leaderboard_id: low_board['id'], user_id: user1['id'] }
 
 # Get all best
-get '/best_scores',                { leaderboard_id: low_board['id'], page_num: 1, num_per_page: 2 }
+get '/v1/best_scores',                { leaderboard_id: low_board['id'], page_num: 1, num_per_page: 2 }
 
 # List achievements
-get '/achievements',               { }
+get '/v1/achievements',               { }
 
 # Get progress of all user 1 achievements for this app:
-get '/achievements',               { user_id: user1['id'] }
+get '/v1/achievements',               { user_id: user1['id'] }
 
 # Update a user
-put "/users/#{user1['id']}",       { gamecenter_id: 12345, fb_id: 12345 }
+put "/v1/users/#{user1['id']}",       { gamecenter_id: 1234500000, fb_id: 12345 }
 
 
