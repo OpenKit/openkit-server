7dd057e06f8af56249275d20c31f8401e962af88
Lou Zell <lzell11@gmail.com>
2013-09-17 11:54:38 -0700

    Comment reminder

--- lib/push_service.rb
+++ lib/push_service.rb
@@ -17,8 +17,8 @@
 # p.connect
 # p.write("7263097dd87a783c5d90dfa61ad3df3d17b11428143c788e77c1be4c2d162d38", {aps: {alert: "This is cool!", badge: 1, sound: "default"}, other_meta: 10})
 #
-# $ gpg --force-mdc -c 1p.txt
-# $ gpg --yes --batch -d --passphrase='password' 1p.txt.gpg
+# $ gpg --force-mdc -c 1_p.txt
+# $ gpg --yes --batch -d --passphrase='password' 1_p.txt.gpg
 #
 # $ bundle exec ruby lib/push_loop.rb
 #
