e02cd2e7dfb600d0be9b04a4ddc26f8693744f08
Lou Zell <lzell11@gmail.com>
2013-12-03 12:06:07 -0800

    Store secret key in redis on app create

--- app/models/app.rb
+++ app/models/app.rb
@@ -18,6 +18,7 @@ class App < ActiveRecord::Base
 
   before_validation :set_app_key, :on => :create
   before_validation :set_secret_key, :on => :create
+  after_create :store_secret_in_redis
 
   validates_presence_of :name, :app_key, :secret_key
   validates_uniqueness_of :name, :scope => :developer_id
@@ -79,5 +80,17 @@ class App < ActiveRecord::Base
 
   def set_secret_key
     self.secret_key = OAuth::Helper.generate_key(40)[0, 40]
+  end
+
+  def store_secret_in_redis
+    OKRedis.connection.set(secret_location, secret_key)
+  end
+
+  def remove_secret_from_redis
+    OKRedis.connection.del(secret_location)
+  end
+
+  def secret_location
+    "sk:#{app_key}"
   end
 end
