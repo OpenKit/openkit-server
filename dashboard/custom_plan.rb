bda8da05c6cac37febe3e3e28710b11ae1ca4b96
Lou Zell <lzell11@gmail.com>
2013-12-05 17:49:56 -0800

    Prevent zeus test from using rake

--- dashboard/custom_plan.rb
+++ dashboard/custom_plan.rb
@@ -1,11 +1,15 @@
 require 'zeus/rails'
 
 class CustomPlan < Zeus::Rails
-
-  # def my_custom_command
-  #  # see https://github.com/burke/zeus/blob/master/docs/ruby/modifying.md
-  # end
-
+  # Overriding the test plan to avoid falling back to rake.
+  # See lib/zeus/rails.rb in the Zeus gem to see default behavior.
+  def test(argv=ARGV)
+    if argv.empty?
+      Zeus::M.run(["test/**/*_test.rb"])
+    else
+      Zeus::M.run(ARGV)
+    end
+  end
 end
 
 Zeus.plan = CustomPlan.new
