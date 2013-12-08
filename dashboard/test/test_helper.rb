3ec0f67b60f2aae6af019742e3ad217ff373ed1e
Lou Zell <lzell11@gmail.com>
2013-12-07 19:29:41 -0800

    test_helper cleanup

--- dashboard/test/test_helper.rb
+++ dashboard/test/test_helper.rb
@@ -1,20 +1,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
+
 require "rails/test_help"
-require 'minitest/spec'
-
-
-# Uncomment for awesome colorful output
-# require "minitest/pride"
-
-class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
-  # Add more helper methods to be used by all tests here...
-end
-
+require 'mocha/setup'
 require 'authlogic/test_case'
 include Authlogic::TestCase
-require 'mocha/setup'
 
 Turn.config do |c|
  c.format  = :outline
