f9f82ec84e8d08bd88e49e271806b5809d6d1f89
Lou Zell <lzell11@gmail.com>
2013-12-10 13:20:59 -0800

    Service controllers subclass ActionController::Metal

--- service/app/controllers/v1/application_controller.rb
+++ service/app/controllers/v1/application_controller.rb
@@ -1,6 +1,64 @@
 module V1
-class ApplicationController < ActionController::Base
-  layout false
+class ApplicationController < ActionController::Metal
+
+  include ActionController::Head
+  include ActionController::Rendering
+  include ActionController::Renderers::All
+  include ActionController::MimeResponds
+  include AbstractController::Callbacks
+  include ActionController::ParamsWrapper
+
+  # For reference:
+  # MODULES = [
+  #   AbstractController::Layouts,
+  #   AbstractController::Translation,
+  #   AbstractController::AssetPaths,
+  #
+  #   ActionController::Helpers,
+  #   ActionController::HideActions,
+  #   ActionController::UrlFor,
+  #   ActionController::Redirecting,
+  #   ActionController::Rendering,
+  #   ActionController::Renderers::All,
+  #   ActionController::ConditionalGet,
+  #   ActionController::RackDelegation,
+  #   ActionController::Caching,
+  #   ActionController::MimeResponds,
+  #   ActionController::ImplicitRender,
+  #   ActionController::StrongParameters,
+  #
+  #   ActionController::Cookies,
+  #   ActionController::Flash,
+  #   ActionController::RequestForgeryProtection,
+  #   ActionController::ForceSSL,
+  #   ActionController::Streaming,
+  #   ActionController::DataStreaming,
+  #   ActionController::RecordIdentifier,
+  #   ActionController::HttpAuthentication::Basic::ControllerMethods,
+  #   ActionController::HttpAuthentication::Digest::ControllerMethods,
+  #   ActionController::HttpAuthentication::Token::ControllerMethods,
+  #
+  #   # Before callbacks should also be executed the earliest as possible, so
+  #   # also include them at the bottom.
+  #   AbstractController::Callbacks,
+  #
+  #   # Append rescue at the bottom to wrap as much as possible.
+  #   ActionController::Rescue,
+  #
+  #   # Add instrumentations hooks at the bottom, to ensure they instrument
+  #   # all the methods properly.
+  #   ActionController::Instrumentation,
+  #
+  #   # Params wrapper should come before instrumentation so they are
+  #   # properly showed in logs
+  #   ActionController::ParamsWrapper
+  # ]
+  #
+  # MODULES.each do |mod|
+  #   include mod
+  # end
+
+  wrap_parameters format: :json
   respond_to :json
   before_filter :require_authorized_app
 
@@ -27,10 +85,10 @@ class ApplicationController < ActionController::Base
   def request_base_uri
     request.protocol + request.host_with_port
   end
-  
+
   def in_sandbox?
     !!request.subdomain.match(/^(?:beta-)?sandbox$/)
   end
-  
+
 end
 end
